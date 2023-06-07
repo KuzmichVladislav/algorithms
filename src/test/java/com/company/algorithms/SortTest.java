78876f8b0b2aa0ad23f4e93cfa61e767df27bfe0
KuzmichV <122460404+KuzmichV@users.noreply.github.com>
2023-06-07 10:58:26 +0300

    test(algorithms): Shell Sort Tests
    add tests for Shell sort with step generator

--- src/test/java/com/company/algorithms/SortTest.java
+++ src/test/java/com/company/algorithms/SortTest.java
@@ -3,6 +3,10 @@ package com.company.algorithms;
 import com.company.algorithms.impl.BubbleSort;
 import com.company.algorithms.impl.InsertionSort;
 import com.company.algorithms.impl.SelectionSort;
+import com.company.algorithms.impl.ShellSort;
+import com.company.algorithms.impl.shell_step.impl.HibbardStep;
+import com.company.algorithms.impl.shell_step.impl.KnutStep;
+import com.company.algorithms.impl.shell_step.impl.ShellStep;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,7 +31,9 @@ class SortTest {
     long endTime = System.nanoTime();
     long duration = (endTime - startTime);
     System.out.println(
-        "Time taken for sorting 100000 elements using Bubble Sort: " + duration + " nanoseconds");
+        "Time taken for sorting 100000 elements using Bubble Sort: "
+            + String.format("%.3f", duration / 1_000_000_000.0)
+            + " seconds");
     for (int i = 0; i < arr.length - 1; i++) {
       Assertions.assertTrue(arr[i] <= arr[i + 1]);
     }
@@ -42,8 +48,8 @@ class SortTest {
     long duration = (endTime - startTime);
     System.out.println(
         "Time taken for sorting 100000 elements using Insertion Sort: "
-            + duration
-            + " nanoseconds");
+            + String.format("%.3f", duration / 1_000_000_000.0)
+            + " seconds");
     for (int i = 0; i < arr.length - 1; i++) {
       Assertions.assertTrue(arr[i] <= arr[i + 1]);
     }
@@ -58,8 +64,72 @@ class SortTest {
     long duration = (endTime - startTime);
     System.out.println(
         "Time taken for sorting 100000 elements using Selection Sort: "
-            + duration
-            + " nanoseconds");
+            + String.format("%.3f", duration / 1_000_000_000.0)
+            + " seconds");
+    for (int i = 0; i < arr.length - 1; i++) {
+      Assertions.assertTrue(arr[i] <= arr[i + 1]);
+    }
+  }
+
+  @Test
+  void testShellSort() {
+    long startTime = System.nanoTime();
+    Sort shellSort = new ShellSort();
+    shellSort.sort(arr);
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    System.out.println(
+        "Time taken for sorting 100000 elements using Shel Sort: "
+            + String.format("%.3f", duration / 1_000_000_000.0)
+            + " seconds");
+    for (int i = 0; i < arr.length - 1; i++) {
+      Assertions.assertTrue(arr[i] <= arr[i + 1]);
+    }
+  }
+
+  @Test
+  void testShellSortWithHibbardStep() {
+    long startTime = System.nanoTime();
+    ShellSort shellSort = new ShellSort();
+    shellSort.modifiedSort(arr, new HibbardStep(arr));
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    System.out.println(
+        "Time taken for sorting 100000 elements using Shell Sort With Hibbard Step: "
+            + String.format("%.3f", duration / 1_000_000_000.0)
+            + " seconds");
+    for (int i = 0; i < arr.length - 1; i++) {
+      Assertions.assertTrue(arr[i] <= arr[i + 1]);
+    }
+  }
+
+  @Test
+  void testShellSortWithShellStep() {
+    long startTime = System.nanoTime();
+    ShellSort shellSort = new ShellSort();
+    shellSort.modifiedSort(arr, new ShellStep(arr));
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    System.out.println(
+        "Time taken for sorting 100000 elements using Shell Sort With Shell Step: "
+            + String.format("%.3f", duration / 1_000_000_000.0)
+            + " seconds");
+    for (int i = 0; i < arr.length - 1; i++) {
+      Assertions.assertTrue(arr[i] <= arr[i + 1]);
+    }
+  }
+
+  @Test
+  void testShellSortWithKnutStep() {
+    long startTime = System.nanoTime();
+    ShellSort shellSort = new ShellSort();
+    shellSort.modifiedSort(arr, new KnutStep(arr));
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    System.out.println(
+        "Time taken for sorting 100000 elements using Shell Sort With Knut Step: "
+            + String.format("%.3f", duration / 1_000_000_000.0)
+            + " seconds");
     for (int i = 0; i < arr.length - 1; i++) {
       Assertions.assertTrue(arr[i] <= arr[i + 1]);
     }
