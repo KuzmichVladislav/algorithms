f2cebbcdf988c66a0d6360d86e1c9e5b87285f80
KuzmichV <122460404+KuzmichV@users.noreply.github.com>
2023-07-25 17:39:06 +0300

    fix(sorting): fixed move elements

--- src/main/java/com/company/algorithms/sorting/impl/InsertionSort.java
+++ src/main/java/com/company/algorithms/sorting/impl/InsertionSort.java
@@ -19,8 +19,10 @@ public class InsertionSort implements Sort {
       // Set j to the index of the previous element
       int j = i - 1;
       // Move elements greater than key to one position ahead of their current position
-      System.arraycopy(arr, j, arr, j + 1, i - j);
-      // Insert key into its correct position in the sorted array
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }      // Insert key into its correct position in the sorted array
       arr[j + 1] = key;
     }
   }
